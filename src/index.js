1cabe61fa37d96abf21ff413cff47dff378ebccb
Edward White <edwardwhite1@outlook.com>
2019-03-20 05:35:38 +0000

    created first GET and POST test setup.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 const api = require('express')()
 const bodyParser = require('body-parser')
 const PORT = 8080
-var hello = "Hello Been API!\n"
+const hello = "Hello Been API!\n"
+const html = "<h1>Whattt? HTML Successful</h1>"
 
-api.get('/first-get', (request, response) => {
-	response.send(hello)
+api.use(bodyParser.json())
+api.use(bodyParser.urlencoded({extended:true})
+)
+
+let posts = [
+	{
+		likes: 6,
+		views: 100,
+		description: 'Look at my morning routine and I pretend I look this good says every girl on IG',
+		comments: [
+			"you look you look dashing",
+			"marry Me pls!",
+			"nice post please check us out for marketing"
+		]
+	},
+	{
+		likes: 5,
+		views: 1200,
+		description: 'Look at my evening routine',
+		comments: [
+			"You look you look great girl. Thanks for being a great friend",
+			"Marry Me pls!",
+			"nice post please check us out for marketing"
+		]
+	}
+]
+
+api.get('/posts', (request, response) => {
+	response.send({
+		posts: posts
+	})
+})
+
+api.post('/post', (req, res) => {
+	posts.push(req.body.post)
+	res.send(posts)
 })
 
 
